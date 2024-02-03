965f8dadddb69d9307e2883f15494ce929c3709f
Abay <abayvmuraleedharan@gmail.com>
2024-02-03 17:11:19 +0530

    1672. Richest Customer Wealth

---
+++ easy/1672.js
@@ -0,0 +1,13 @@
+// 1672. Richest Customer Wealth
+
+var maximumWealth = function(accounts) {
+    let maxWealth = 0;
+    for(let i=0; i<accounts.length; i++){
+        let currentWealth = 0;
+        for(let j=0; j<accounts[i].length; j++){
+            currentWealth += accounts[i][j];
+        }
+        maxWealth = Math.max(maxWealth, currentWealth);
+    }
+    return maxWealth;
+};

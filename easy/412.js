3e0c59255ebc54022b6bf9285de057653e56d298
Abay <abayvmuraleedharan@gmail.com>
2024-03-11 11:31:17 +0530

    412. Fizz Buzz

---
+++ easy/412.js
@@ -0,0 +1,18 @@
+// 412. Fizz Buzz
+
+var fizzBuzz = function(n) {
+    let answer = [];
+    for(let i=1; i<=n; i++){
+        if(i%3==0 && i%5==0){
+            answer.push("FizzBuzz");
+        }else if(i%3==0){
+            answer.push("Fizz");
+        }else if(i%5==0){
+            answer.push("Buzz");
+        }else{
+            let numStr = i.toString();
+            answer.push(numStr);
+        }
+    }
+    return answer;
+};
